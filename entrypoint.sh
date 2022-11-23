33064e1829592d1886b321807d046469ceea5ad0
Andy Qua <andy.qua@gmail.com>
2022-11-23 13:31:59 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,12 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
+ls -la
+echo '-----------'
+ls -la ..
+echo "file - $FILE"
+echo "AWS_S3_BUCKET - $AWS_S3_BUCKET"
+echo "AWS_REGION - $AWS_REGION"
 aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
             --region ${AWS_REGION} $*
 
